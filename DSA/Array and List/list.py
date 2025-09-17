2fc1052e106c81243bca525cc96730e3f7d85b4f
MelvinAnthony <23cp0310003@student.hindustanuniv.ac.in>
2025-09-18 01:47:38 +0530

    list pratice

--- DSA/Array and List/list.py
+++ DSA/Array and List/list.py
@@ -1,10 +1,35 @@
-import sys
+# Create
+arr = [10, 20, 30, 40]
 
-a = []
-print(sys.getsizeof(a))  # memory of empty list
+# Access
+print("Index 2:", arr[2])
 
-a = [1]
-print(sys.getsizeof(a))
+# Update
+arr[1] = 99
+print("After update:", arr)
 
-a = [1, 2]
-print(sys.getsizeof(a))
+# Append
+arr.append(50)
+print("After append:", arr)
+
+# Insert at index 1
+arr.insert(1, 77)
+print("After insert:", arr)
+
+# Delete last
+arr.pop()
+print("After pop remove last value default:", arr)
+
+## delete second value
+
+arr.pop(3)
+print("After pop remove 3 value on list usnig index:", arr)
+
+## search the element
+
+search = 99 in arr
+print(f"True or False value present or not: {search}")
+
+# Length
+print("Length of arr:", len(arr))
+
